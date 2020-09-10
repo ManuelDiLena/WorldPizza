0a8fc239e27dcccef58adf9d6795da5f03e10c2a
ManuelDiLena <manuel.dilena.29@gmail.com>
2020-09-10 18:57:33 -0300

    Contact section: validation

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ var btnMenuOpen = document.getElementById("btnMenuOpen"),
         menuResponsive.classList.remove("active");
     });
 
+
 //Products Slider
 var wrapper = document.querySelector('.slider'),
     btnLeft = document.getElementById("btnLeft"),
@@ -34,3 +35,28 @@ var wrapper = document.querySelector('.slider'),
     btnLeft.addEventListener("click", function() {
         wrapper.scrollLeft -= wrapper.offsetWidth;
     });
+
+
+//Form Validation
+var form = document.getElementById("form");
+
+    function validate(e) {
+        var inputName = document.getElementById("name"),
+            inputEmail = document.getElementById("email"),
+            inputComments = document.getElementById("comments");
+
+        if (inputName.value == 0 || inputEmail.value == 0 || inputComments.value == 0) {
+            //e.preventDeFault();
+            alert("Datos vacíos");
+        }else {
+            //e.preventDeFault();
+            alert("Datos enviados");
+            inputName.value = "";
+            inputEmail.value = "";
+            inputComments.value = "";
+        }
+    };
+
+
+// Form events
+form.addEventListener("submit", validate);
